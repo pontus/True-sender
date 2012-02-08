d6270c533894920071b1f4b4f807c9cbf544d0b5
Pontus Freyhult <pontus_git@soua.net>
2012-02-08 15:47:49 +0100

    Basic design.

--- True sender/src/net/soua/truesender/TruesenderActivity.java
+++ True sender/src/net/soua/truesender/TruesenderActivity.java
@@ -2,12 +2,49 @@ package net.soua.truesender;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.ArrayAdapter;
+
 
 public class TruesenderActivity extends Activity {
     /** Called when the activity is first created. */
+	
+
+    protected ArrayAdapter<CharSequence> operatorAdapter;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+
+        this.operatorAdapter = ArrayAdapter.createFromResource(this, R.array.operators,
+                android.R.layout.simple_spinner_dropdown_item);
+        
+        
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        spinner.setAdapter(this.operatorAdapter);
+        
+
+        Button button = (Button) findViewById(R.id.send_button);
+
+        
     }
+
+ 
+    
+    
+        public void onSend(View v) {
+            // Perform action on click
+        }
+
+        public void onSettings(View v) {
+            // Perform action on click
+        }
+
+
 }
