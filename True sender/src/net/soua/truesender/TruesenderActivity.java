5c8962c9b60e03256cdec02a00c59e12c4ba3f6a
Pontus Freyhult <pontus_truesender@soua.net>
2012-02-08 14:29:44 +0100

    Created project.

---
+++ True sender/src/net/soua/truesender/TruesenderActivity.java
@@ -0,0 +1,13 @@
+package net.soua.truesender;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TruesenderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
