66e6e4bf8d77adefd791ca8f28996f8701ff570e
Pontus Freyhult <pontus_git_commit@soua.net>
2012-02-10 21:44:23 +0100

    Added operator usage.

--- True sender/src/net/soua/truesender/TruesenderActivity.java
+++ True sender/src/net/soua/truesender/TruesenderActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 
-import android.widget.TextView;
 import android.widget.EditText;
 
 import android.content.SharedPreferences;
@@ -16,117 +15,122 @@ import android.content.DialogInterface;
 import android.os.AsyncTask;
 
 public class TruesenderActivity extends Activity {
-    /** Called when the activity is first created. */
-	
-	
+	/** Called when the activity is first created. */
+
 	protected ProgressDialog dialog;
 	protected String send_error;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
-        checkFirstRun();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-        
-    }
+		checkFirstRun();
+	}
 
- 
-    
-    
-        public void onSend(View v) {
-        	
-        	if (!settingsOk())
-        	{
-        		startActivity(new Intent(TruesenderActivity.this, TruesenderSettingsActivity.class));
-        		return;
-        	}
-        	
-	    	SharedPreferences settings = getSharedPreferences( getString(R.string.app_name), MODE_PRIVATE);
-        	
-           	EditText message = (EditText) findViewById(R.id.message);
-        	
-           	dialog = ProgressDialog.show(this, "", 
-                    "Sending. Please wait...", true);
-           	
-        	
-            class SendTask extends AsyncTask<String, Void, Integer> {
-                protected Integer doInBackground(String... msg) {
-                	try { 
-                		Thread.sleep(4000);
-                	}
-                	catch(InterruptedException e)
-                	{
-                		
-                	}
-                	return 1;
-                }
+	class SendTask extends AsyncTask<String, Void, Integer> {
+		@Override
+		protected Integer doInBackground(String... msg) {
 
-                protected void onPostExecute(Integer result) {
-                    //if (result > 0)
-                    {
-                 	   new AlertDialog.Builder(TruesenderActivity.this)
-             	      .setMessage(getString(R.string.send_failed)+send_error)
-             	      .setTitle(getString(R.string.app_name))
-             	      .setCancelable(true)   
-             	      .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
-             	           public void onClick(DialogInterface dialog, int id) {
-             	                dialog.cancel();
-             	           }})
-             	      
-             	      .show();
-	
-                    }
-                    
-                    	
-                    dialog.dismiss();
-                }}
-                
-            	new SendTask().execute(message.getText().toString());
+			SharedPreferences settings = getSharedPreferences(
+					getString(R.string.app_name), MODE_PRIVATE);
 
-            
-            }
+			String operatorName = settings.getString("operator", "");
+			String classname = "net.soua.truesender."
+					+ operatorName.replaceAll("\\s", "") + "Operator";
 
-        	
-        
+			try {
 
+				OperatorInterface operator = (OperatorInterface) Class.forName(
+						classname).newInstance();
+			} catch (ClassNotFoundException e) {
+				send_error = "Weird, class not found. Class was "+classname+". Corrupt installation/preferences?";
+				return 1;
 
-        
-        public void onSettings(View v) {
-        		startActivity(new Intent(TruesenderActivity.this, TruesenderSettingsActivity.class));
-        }
+			} catch (IllegalAccessException e) {
+				send_error = "Weird, illegal access. Corrupt installation/preferences?";
+				return 1;
+			} catch (InstantiationException e) {
+				send_error = "Weird, instantiation error. Corrupt installation/preferences?";
+				return 1;
+			}
 
+			return 0;
+		}
 
-        private boolean settingsOk() {
-	    	SharedPreferences settings = getSharedPreferences( getString(R.string.app_name), MODE_PRIVATE);
+		@Override
+		protected void onPostExecute(Integer result) {
+			if (result > 0) {
+				new AlertDialog.Builder(TruesenderActivity.this)
+						.setMessage(
+								getString(R.string.send_failed) + send_error)
+						.setTitle(getString(R.string.app_name))
+						.setCancelable(true)
+						.setNeutralButton("Ok",
+								new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick(DialogInterface dialog,
+											int id) {
+										dialog.cancel();
+									}
+								})
 
-	    	// If operator is set, all is well
-	    	return (!settings.getString("operator","").equals(""));
-	    		
-                
-        }
+						.show();
 
-        
-        
-        private void checkFirstRun() {
+			}
 
-        	if (settingsOk())
-        			return;
-        	
-    	   new AlertDialog.Builder(this)
-        	      .setMessage(getString(R.string.long_welcome))
-        	      .setTitle(getString(R.string.welcome))
-        	      .setCancelable(true)   
-        	      .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
-        	           public void onClick(DialogInterface dialog, int id) {
-        	                dialog.cancel();
-        	        		startActivity(new Intent(TruesenderActivity.this, TruesenderSettingsActivity.class));
-        	           }})
-        	      
-        	      .show();
-        	   
-        }
-       
+			dialog.dismiss();
+		}
+	}
 
+	public void onSend(View v) {
+
+		if (!settingsOk()) {
+			startActivity(new Intent(TruesenderActivity.this,
+					TruesenderSettingsActivity.class));
+			return;
+		}
+
+		SharedPreferences settings = getSharedPreferences(
+				getString(R.string.app_name), MODE_PRIVATE);
+
+		EditText message = (EditText) findViewById(R.id.message);
+
+		dialog = ProgressDialog.show(this, "", "Sending. Please wait...", true);
+
+		new SendTask().execute(message.getText().toString());
+
+	}
+
+	public void onSettings(View v) {
+		startActivity(new Intent(TruesenderActivity.this,
+				TruesenderSettingsActivity.class));
+	}
+
+	private boolean settingsOk() {
+		SharedPreferences settings = getSharedPreferences(
+				getString(R.string.app_name), MODE_PRIVATE);
+
+		// If operator is set, all is well
+		return (!settings.getString("operator", "").equals(""));
+	}
+
+	private void checkFirstRun() {
+
+		if (settingsOk())
+			return;
+
+		new AlertDialog.Builder(this)
+				.setMessage(getString(R.string.long_welcome))
+				.setTitle(getString(R.string.welcome)).setCancelable(true)
+				.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+						startActivity(new Intent(TruesenderActivity.this,
+								TruesenderSettingsActivity.class));
+					}
+				}).show();
+
+	}
 }
